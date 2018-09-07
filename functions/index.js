34cdc87c2c63aa11119bc882d3912599ad2c9d21
zainmustafa <zmdhillo@gmail.com>
2018-09-08 02:07:21 +0500

    first version deployed.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
-const functions = require('firebase-functions');
-
+const functions = require("firebase-functions");
+const schedule = require("node-schedule");
+const admin = require("firebase-admin");
+admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  schedule.scheduleJob("0 17 ? * 0,4-6", () => {
+    console.log("The answer to life, the universe, and everything!");
+    admin
+      .database()
+      .ref(`current`)
+      .set({ name: count++, url: cout++ });
+  });
+});
