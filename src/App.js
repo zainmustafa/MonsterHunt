a4061581a35dcc908ea6706d13dc0e98cf32d673
zainmustafa <zmdhillo@gmail.com>
2018-09-08 03:31:12 +0500

    update page src.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import logo from "./one.png";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img className="app-img" src={logo} alt="New file" />
+        <img className="app-img" src="https://firebasestorage.googleapis.com/v0/b/monster-hunt-ar.appspot.com/o/Screenshot_1536333050.png?alt=media&token=eddd90a9-6bc6-42b1-8d55-730b246294f5" alt="New file" />
       </div>
     );
   }
