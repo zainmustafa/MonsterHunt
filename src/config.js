341ff80090e787ce1e84ee1c01b72e1f2bfab95c
zainmustafa <zmdhillo@gmail.com>
2018-09-08 14:00:59 +0500

    add firebase.

---
+++ src/config.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+const config = {
+    apiKey: "AIzaSyAuQwnkJaaNwiFaTv7xvFNpVLbwdwdsVEE",
+    authDomain: "monster-hunt-ar.firebaseapp.com",
+    databaseURL: "https://monster-hunt-ar.firebaseio.com",
+    projectId: "monster-hunt-ar",
+    storageBucket: "monster-hunt-ar.appspot.com",
+    messagingSenderId: "599750708002"
+  };
+firebase.initializeApp(config);
