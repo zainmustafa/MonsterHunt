6293184f61b2f11b0c43148513c1063ed3ca1f73
zainmustafa <zmdhillo@gmail.com>
2018-09-08 14:01:16 +0500

    update cloud function.

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,30 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
+const entries = require("lodash/entries");
+
+
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.updateimage = functions.https.onRequest((request, response) => {
-  admin
-  .database()
-  .ref(`current`)
-  .set({ name: 1, url: 1 },()=>{
-    response.send("Data Set")
-  })
-  
+
+exports.updateimage = functions.https.onRequest((req, res) => {
+  return admin
+    .database()
+    .ref("/Images")
+    .once("value")
+    .then(snapshot => {
+      var arr = entries(snapshot.val()).map(([key, value]) => ({
+        key,
+        value
+      }));
+      const random = Math.floor(Math.random() * arr.length);
+      return admin
+        .database()
+        .ref(`current`)
+        .set({ name: arr[random].key, url: arr[random].value }, () => {
+          res.send("Image Update");
+        });
+    });
 });
